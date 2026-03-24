9560bf94c53a6bb6319b34317d393380d5205f8a
PPAvdK <philipkoolwijk@hotmail.com>
2026-03-24 22:39:02 +0100

    Update listonic.js

--- netlify/functions/listonic.js
+++ netlify/functions/listonic.js
@@ -12,15 +12,16 @@ exports.handler = async (event) => {
     return { statusCode: 200, headers: cors, body: '' };
   }
 
-  const path = (event.queryStringParameters || {}).path || '/projects';
-  const auth = event.headers.authorization || '';
+  const params = event.queryStringParameters || {};
+  const path = params.path || '/projects';
+  const token = params.token || (event.headers.authorization || '').replace('Bearer ', '');
 
   const options = {
     hostname: 'api.todoist.com',
     path: '/api/v1' + path,
     method: event.httpMethod,
     headers: {
-      'Authorization': auth,
+      'Authorization': 'Bearer ' + token,
       'Content-Type': 'application/json'
     }
   };
@@ -30,11 +31,7 @@ exports.handler = async (event) => {
       let data = '';
       res.on('data', c => data += c);
       res.on('end', () => {
-        resolve({
-          statusCode: res.statusCode,
-          headers: cors,
-          body: data
-        });
+        resolve({ statusCode: res.statusCode, headers: cors, body: data });
       });
     });
     req.on('error', (e) => {
@@ -44,3 +41,8 @@ exports.handler = async (event) => {
     req.end();
   });
 };
+```
+
+Commit → dan test je met:
+```
+https://imaginative-empanada-61637f.netlify.app/.netlify/functions/listonic?path=/projects&token=b637cf1d66210628931915e1c694bdbd636d98
